f4e32cf6837d258b74b487acccc1e3b54a1da25b
Peter Schenkels <43245077+Peter-Schenkels@users.noreply.github.com>
2019-12-27 14:40:26 +0100

    Update midi_to_notes.py

--- midi_to_notes.py
+++ midi_to_notes.py
@@ -2,20 +2,21 @@ import mido
 from note import note
 
 def midi_to_notes(fileLocation):
+    
     song = []
     mid = mido.MidiFile(fileLocation)
-
     currLow = 1000
+    
     for event in mid:
 
         message = str(event)
         type = message.split()[0]
+        
         if type == "note_on" or type == "note_off":
 
             music_note = message.split()[2].replace("note=", "")
-
-
             length = message.split()[4].replace("time=", "")
+            
             if type == "note_off":
                 song.append(note('x', length))
             else:
@@ -24,6 +25,7 @@ def midi_to_notes(fileLocation):
                     currLow = int(music_note)
 
     for musicn in song:
+        
         if musicn.name != 'x':
             musicn.name = str(int(musicn.name) - currLow)
 
